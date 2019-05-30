f88fa98fe37a27439d1460ab5051de09cb4cd5f7
Ross McGuyer <RAMcGuyer@gmail.com>
2019-05-30 13:54:30 -0700

    added coord2D.hpp

---
+++ c_src/Coord2D.hpp
@@ -0,0 +1,58 @@
+
+#ifndef __COORD2D_H__
+#define __COORD2D_H__
+public class Coord2D {
+//Coord2D is a coordinate object that enables us to keep track of
+// which tiles in our grid our landmarks.
+    private int x;
+    private int y;
+
+//Constructors, take in an X and a Y coordinate.
+    public Coord2D(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
+// Constructor that creates a deep copy of a coordinate object
+    public Coord2D(Coord2D other) {
+        
+        this.x = other.x;
+        this.y = other.y;
+    }
+//Getters    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+
+//Displays contents of object to the console.
+    public String toString() {
+        
+        return "{" + Integer.toString(x) + ", " + Integer.toString(y) + "}";
+    }
+
+//Comparator function    
+    public bool equals(Object other) {
+        
+//        return this.x == other.x && this.y == other.y;
+        if (other instanceof Coord2D) {
+            
+            Coord2D otherCoord = (Coord2D) other;
+            
+            return this.x == otherCoord.x && this.y == otherCoord.y;
+        }
+        
+        else return false;
+    }
+
+//For use in the hashset
+    public int hashCode() {
+        
+        return x + y;
+    }
+    
+}
+#endif
